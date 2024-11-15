83911a3174be986d6d16c85ee2085cca594aa0ee
Damian Escobar <damianescobar2001@gmail.com>
2024-11-15 14:40:18 -0300

    Primer commit

---
+++ src/TaskList.jsx
@@ -0,0 +1,29 @@
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
+
+import React from "react";
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  if (tasks.length === 0) {
+    return <h1>No hay tareas aun</h1>;
+  }
+
+  return (
+    <div>
+      {tasks.map((task,i) => (
+        <div key={i}>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
