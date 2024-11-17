47a3e71d3f177fa67a79de9fb8a0f2c40b2592f9
Damian Escobar <damianescobar2001@gmail.com>
2024-11-17 12:03:37 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://DamianEscobar.github.io/React-vite-fazt-tasks'
 })
